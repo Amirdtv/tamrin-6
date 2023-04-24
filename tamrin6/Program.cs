f5a584150504967e46238a89baae2659c9484e5a
Amir Hossein Amanipoor <76044261+Amirdtv@users.noreply.github.com>
2023-04-25 00:28:07 +0330

    s

---
+++ tamrin6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin6
+{
+    internal class Program
+    {
+        private static int test (int x , int y)
+        {
+            double result;
+            result = (Math.Sqrt(Math.Abs(x))) + (Math.Pow(y, 2));
+            Console.WriteLine( result );
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" enter numbers ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+            test (x,y);
+        }
+    }
+}
